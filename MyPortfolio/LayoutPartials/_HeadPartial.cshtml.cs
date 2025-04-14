9ef0974ee5a4500287bbe6a5b222e34a351b7bad
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2025-04-14 23:28:23 +0300

    refactor: edited Layout ındex page using partial view

---
+++ MyPortfolio/LayoutPartials/_HeadPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyPortfolio.LayoutPartials
+{
+    public class _HeadPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
