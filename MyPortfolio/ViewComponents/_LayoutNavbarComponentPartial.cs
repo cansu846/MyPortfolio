c8ecc142a3da9be68b95ba9a5bb2502bcb6b0d0f
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2025-04-14 23:19:59 +0300

    refactor: edited layout index page using viewcomponent

---
+++ MyPortfolio/ViewComponents/_LayoutNavbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _LayoutNavbarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();  
+        }
+    }
+}
